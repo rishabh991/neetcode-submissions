665ae00e4de67faffb9a9174dce47a25304e1cb9
Rishabh Gupta <rishabh.qc991@gmail.com>
2026-04-15 04:03:44 +0530

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0; i<prices.length; i++){
+            min = Math.min(prices[i],min);
+            max = Math.max(prices[i]-min, max);
+        }
+        return max;
+    }
+}
